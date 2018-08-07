d0f34f67f84d79d7df93baa7773caf7a04a1a9d3
Magda Gutowska <m.gutowska0909@gmail.com>
2018-08-07 23:19:56 +0200

    Add files

---
+++ actions.js
@@ -0,0 +1,42 @@
+import uuid from 'uuid';
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+const addComment = (text) => ({
+  type: ADD_COMMENT,
+  text,
+  id: uuid.v4()
+});
+
+const removeComment = (id) => ({
+  type: REMOVE_COMMENT,
+  id
+});
+
+const editComment = (id, text) => ({
+  type: EDIT_COMMENT,
+  text,
+  id
+});
+
+const thumbUpComment = (id) => ({
+  type: THUMB_UP_COMMENT,
+  id
+});
+
+const thumbDownComment = (id) => ({
+  type: THUMB_DOWN_COMMENT,
+  id
+});
+
+export default {
+  addComment,
+  removeComment,
+  editComment,
+  thumbUpComment,
+  thumbDownComment
+}
---
+++ recuder-compiled.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
+
+const app = combineReducers({
+  comments,
+  users
+});
+
+export default app;
+
+//# sourceMappingURL=recuder-compiled.js.map
